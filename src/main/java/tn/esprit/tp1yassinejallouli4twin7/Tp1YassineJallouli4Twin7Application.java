bd8748ed4a9e5c8ff9e48ffcb5dbd0290e0a68e9
nejdbedoui <nejdbedoui@gmail.com>
2024-03-30 17:41:29 +0100

    add matrics

--- src/main/java/tn/esprit/tp1yassinejallouli4twin7/Tp1YassineJallouli4Twin7Application.java
+++ src/main/java/tn/esprit/tp1yassinejallouli4twin7/Tp1YassineJallouli4Twin7Application.java
@@ -1,7 +1,12 @@
 package tn.esprit.tp1yassinejallouli4twin7;
 
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.CollectorRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,4 +20,8 @@ public class Tp1YassineJallouli4Twin7Application {
         SpringApplication.run(Tp1YassineJallouli4Twin7Application.class, args);
     }
 
+    @Bean
+    public PrometheusMeterRegistry prometheusMeterRegistry() {
+        return new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    }
 }
---
+++ src/main/java/tn/esprit/tp1yassinejallouli4twin7/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package tn.esprit.tp1yassinejallouli4twin7.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
