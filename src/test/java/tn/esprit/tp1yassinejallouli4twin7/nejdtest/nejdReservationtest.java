fbd1b75afb9dd3cfe85cd4e0460d6567b2ecdadf
nejdbedoui <74482153+nejdbedoui@users.noreply.github.com>
2024-04-14 11:41:41 +0100

    Update nejdReservationtest.java

--- src/test/java/tn/esprit/tp1yassinejallouli4twin7/nejdtest/nejdReservationtest.java
+++ src/test/java/tn/esprit/tp1yassinejallouli4twin7/nejdtest/nejdReservationtest.java
@@ -1,6 +1,6 @@
 package tn.esprit.tp1yassinejallouli4twin7.nejdtest;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
