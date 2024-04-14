5e8bd7c4e38e4e7c9e937730572e5e0083c22e05
Jallouli-Yassine <jungloul1@gmail.com>
2024-04-15 00:42:25 +0100

    adjust code

--- src/test/java/tn/esprit/tp1yassinejallouli4twin7/Tp1YassineJallouli4Twin7ApplicationTests.java
+++ src/test/java/tn/esprit/tp1yassinejallouli4twin7/Tp1YassineJallouli4Twin7ApplicationTests.java
@@ -1,13 +1,45 @@
 package tn.esprit.tp1yassinejallouli4twin7;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tn.esprit.tp1yassinejallouli4twin7.entities.Foyer;
+import tn.esprit.tp1yassinejallouli4twin7.repositories.IFoyerRepo;
+import tn.esprit.tp1yassinejallouli4twin7.services.IFoyerServices;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class Tp1YassineJallouli4Twin7ApplicationTests {
 
+    @Autowired
+    private IFoyerServices foyerService;
+
+    @Autowired
+    private IFoyerRepo foyerRepository;
+
     @Test
     void contextLoads() {
+        assertNotNull(foyerService);
+
     }
 
+    @Test
+    void testAjouterFoyer() {
+        // Given
+        Foyer foyerToAdd = new Foyer(); // Create a sample Foyer object
+
+        // When
+        Foyer savedFoyer = foyerService.ajouterFoyer(foyerToAdd); // Call the method to be tested
+
+        // Then
+        assertNotNull(savedFoyer); // Verify that the returned Foyer object is not null
+        Foyer retrievedFoyer = foyerRepository.findById(savedFoyer.getIdFoyer()).orElse(null); // Retrieve the saved Foyer from the repository
+        assertNotNull(retrievedFoyer); // Verify that the retrieved Foyer is not null
+        assertEquals(foyerToAdd, retrievedFoyer); // Verify that the retrieved Foyer matches the input Foyer
+    }
 }
